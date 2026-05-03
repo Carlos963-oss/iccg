ae4c607c159fc0d426a4e3e71d842b1abcc954b6
carlos <p9908974@gmail.com>
2026-05-03 16:36:43 -0500

    create superuser auto

--- iccg/settings.py
+++ iccg/settings.py
@@ -150,3 +150,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.up.railway.app",
 ]
 
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@gmail.com", "12345678")
+
